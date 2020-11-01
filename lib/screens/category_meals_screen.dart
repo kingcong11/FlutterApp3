278bfb04195b98130b53c382f866998970290829
Kingcong11 <nico.ativo@bicol-u.edu.ph>
2020-11-01 21:14:58 +0800

    test

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -26,10 +26,10 @@ class CategoryMealsScreen extends StatelessWidget {
   }
 
   // /* Getters */
-  // double _getRemainingContent(MediaQueryData _mediaQuery, AppBar appbar) {
-  //   return (_mediaQuery.size.height -
-  //       (appbar.preferredSize.height + _mediaQuery.padding.top));
-  // }
+  double _getRemainingContent(MediaQueryData _mediaQuery, AppBar appbar) {
+    return (_mediaQuery.size.height -
+        (appbar.preferredSize.height + _mediaQuery.padding.top));
+  }
 
   @override
   Widget build(BuildContext context) {
